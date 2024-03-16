dc0b393c9d58d28e6e27f42499544471c2febfcf
Harshavardhan_T <117514867+Harshavardhan1007@users.noreply.github.com>
2024-03-16 18:07:16 +0530

    spotify_transformation_load_function.py

--- spotify_transformation_load_function.py
+++ spotify_transformation_load_function.py
@@ -1,3 +1,67 @@
+"""
+Transforms Spotify data extracted from Amazon S3 and loads it into different S3 buckets.
+
+This AWS Lambda function is triggered when new data is dumped into the 'extract' S3 bucket.
+It extracts track information from a specified Spotify playlist, cleans the data, formats it properly,
+and loads it into different S3 buckets for further processing.
+
+Environment Variables:
+- client_id: Spotify API client ID stored as an environment variable.
+- client_secret: Spotify API client secret stored as an environment variable.
+
+Dependencies:
+- spotipy: A lightweight Python library for the Spotify Web API.
+- boto3: The AWS SDK for Python, used for interacting with Amazon S3.
+
+Execution Steps:
+1. Retrieves Spotify API client ID and client secret from environment variables.
+2. Creates a Spotify client object using the client credentials manager.
+3. Fetches playlist information from the Spotify API using a predefined playlist URI.
+4. Converts the retrieved data to JSON format.
+5. Initializes an S3 client object.
+6. Generates a unique file name using the current timestamp.
+7. Uploads the JSON data to the specified 'raw_data/to_be_processed/' directory in the 'spotify-etl-project-2024' S3 bucket.
+
+Note:
+- This function assumes that the specified playlist URI corresponds to a publicly accessible Spotify playlist.
+- The AWS Lambda function must have appropriate permissions to interact with the specified S3 bucket.
+- This function is designed to be triggered when new data is dumped into the 'extract' S3 bucket.
+"""
+
+# Importing necessary modules
+import json
+import os
+import spotipy
+import boto3
+from datetime import datetime
+from spotipy.oauth2 import SpotifyClientCredentials
+
+def lambda_handler(event, context):
+    # Retrieving client ID and client secret from environment variables
+    client_id = os.environ.get('client_id')
+    client_secret = os.environ.get('client_secret')
+    
+    # Creating a Spotify client object
+    client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    
+    # Fetching playlist information from Spotify API
+    playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
+    playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+    spotify_data = sp.playlist_tracks(playlist_URI)
+    
+    # Initializing S3 client
+    s3_client = boto3.client('s3')
+    
+    # Generating file name using current timestamp
+    file_name = "spotify_raw_" + str(datetime.now()) + ".json"
+    
+    # Uploading JSON data to S3 bucket
+    s3_client.put_object(
+        Bucket="spotify-etl-project-2024",
+        Key="raw_data/to_be_processed/" + file_name,
+        Body=json.dumps(spotify_data)
+    )
 import json
 import boto3
 from datetime import datetime
